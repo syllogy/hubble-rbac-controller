8026117301c870a1e0d8bfff09eb891a5f55f532
Jimmy Rasmussen <jwr@lunar.app>
2020-06-08 15:05:43 +0200

    Ensure domain contains a dot

--- pkg/apis/lunarway/v1alpha1/doc.go
+++ pkg/apis/lunarway/v1alpha1/doc.go
@@ -1,4 +1,4 @@
-// Package v1alpha1 contains API Schema definitions for the lunarway v1alpha1 API group
+// Package v1alpha1 contains API Schema definitions for the lunarway.com v1alpha1 API group
 // +k8s:deepcopy-gen=package,register
-// +groupName=lunarway
+// +groupName=lunarway.com
 package v1alpha1
